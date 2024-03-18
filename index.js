23c6cd36b17b33d02cc575135e78425e764e2501
Pandey023 <162384779+Pandey023@users.noreply.github.com>
2024-03-18 20:52:01 +1100

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 
-app.get('/multiplyTwoNumbers', function (req, res) {
+app.get('/subTwoNumbers', function (req, res) {
     let num1 = parseInt(req.query.num1);
     let num2 = parseInt(req.query.num2);
     
@@ -15,7 +15,7 @@ app.get('/multiplyTwoNumbers', function (req, res) {
         return res.status(400).json({ message: 'Invalid input. Please provide valid numbers' });
     }
 
-    let result = num1 * num2;
+    let result = num1 - num2;
     let response = { data: result, message: 'success', statusCode: 200 };
     res.json(response);
 });
